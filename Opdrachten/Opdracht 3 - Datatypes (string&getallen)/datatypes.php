1a5bb4cbe003fee238091b34b8ed69dce9fe5634
Bradley-Zandgrond <9021118@student.zadkine.nl>
2023-12-11 13:06:54 +0100

    Create datatypes.php

---
+++ Opdrachten/Opdracht 3 - Datatypes (string&getallen)/datatypes.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>datastypes</title>
+</head>
+<body>
+<?php
+
+$bak1 = "hoi";
+$bak2 = "hoe gaat het";
+$getal1 = 1;
+$getal2 = 2;
+
+echo "$bak1";
+
+echo "<br>";
+
+echo "$bak2";
+
+echo "<br>";
+
+echo "$getal1";
+
+echo "<br>";
+
+echo "$getal2";
+
+?>
+</body>
+</html>
