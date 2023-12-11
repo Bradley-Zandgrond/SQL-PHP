0b8b6e867d4677bd47f2b4855c567602aa1401b0
Bradley-Zandgrond <9021118@student.zadkine.nl>
2023-12-11 13:13:56 +0100

    Create dagdeel.php

---
+++ Opdrachten/Opdracht 4 - H4 opdracht 1 tm 4/dagdeel.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dagdeel</title>
+</head>
+<body>
+<?php
+    $time = 6;
+
+    if ($time >= 6 && $time <= 12) {
+        echo "het is ochtend";
+    } elseif ($time > 12 && $time <= 18) {
+        echo "het is middag";
+    } elseif ($time > 18 && $time <= 24) {
+        echo "het is avond";
+    } else {
+        echo "het is nacht";
+    }
+?>
+</body>
+</html>
