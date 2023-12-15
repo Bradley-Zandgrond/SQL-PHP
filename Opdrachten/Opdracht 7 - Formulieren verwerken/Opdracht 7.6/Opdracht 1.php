52a5a9db44a531e3c0eae41fd8090de07e95fccf
Bradley-Zandgrond <9021118@student.zadkine.nl>
2023-12-15 08:37:41 +0100

    Update Opdracht 1.php

--- Opdrachten/Opdracht 7 - Formulieren verwerken/Opdracht 7.6/Opdracht 1.php
+++ Opdrachten/Opdracht 7 - Formulieren verwerken/Opdracht 7.6/Opdracht 1.php
@@ -22,6 +22,42 @@
 if(isset($_POST)) {
     //var_dump($_POST);
 
+    // validate bedrag
+    if (isset($_POST['bedrag']) == true &&
+    strlen($_POST['bedrag']) > 0 &&
+    filter_var($_POST['bedrag'], FILTER_VARLIDATE_FLOAT)){
+        echo "het geposte bedrag is:" . $_POST['bedrag'];
+    } else {
+        echo "Error: bedrag is niet ingevuld<br>";
+    }
+
+
+
+
+    if (isset($_POST['omzetten'])) {
+        
+        // validate bedrag
+        if (strlen($_POST['bedrag']) == 0){
+            echo "Error: bedrag is niet ingevuld<br>";
+        } elseif (filter_var($_POST['bedrag'], FILTER_VALIDATE_FLOAT) == false) {
+            echo "Error: bedrag " . $_POST['bedrag'] . "is niet ok<br>";
+        } else {
+            // Getal is ok
+            echo "Het geposte bedrag is:" . $_POST['bedrag'] . "<br>"
+        }
+
+
+
+
+
+
+
+    }
+
+    // check of BTW is aangeklikt
+
+
+
     if ($_POST['BTW'] == "negen") {
         $percentage = 1,09;
     } else {
